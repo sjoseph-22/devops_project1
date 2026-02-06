750056f5ae2d59d4074448d053749892f1d0b2a4
Saurabh Joseph <officialsjo22@gmail.com>
2026-02-06 21:48:23 +0100

    Update statiCodeAnalysis.groovy

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -1,7 +1,7 @@
 def call(String credentialsId) {
     echo "Running SonarQube Analysis..."
     // 'SonarQube' must match the name in Manage Jenkins -> System -> SonarQube Server
-    withSonarQubeEnv('SonarQube') {
+    withSonarQubeEnv('sonarqube-api') {
         sh "mvn sonar:sonar -Dsonar.login=${credentialsId}"
     }
 }
